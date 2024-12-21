13e359521d73e484ab206399bc0eada376ec5358
Kyle Rego <regoky@outlook.com>
2024-12-21 13:00:09 -0800

    Add favicon and initial page with video

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Lilgobslayerguides.NET</h1>
+
+        <p>Practical Old School Runescape Ultimate Iron guides for Gielnor's most bankless gamers.</p>
+
+        <section>
+          <h2>The Mess (cooking training)</h2>
+
+          <p></p>
+
+          <figure>
+              <video controls width="1080">
+                <source src="/public/themesscookingpizzasDec12_2024.mp4" type="video/mp4" />
+              </video>
+
+          <figcaption>This is me cooking servery pineapple pizzas for about 100k cooking xp/hr
+            (misclicks were only because of the lag from running the screen recorder):</figcaption>
+
+          </figure>
+        
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App
