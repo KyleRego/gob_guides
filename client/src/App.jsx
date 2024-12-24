4f4f849def9044480501e18587ff9f312cbe9154
Kyle Rego <regoky@outlook.com>
2024-12-24 12:47:37 -0800

    Improve content descriptions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,15 +11,14 @@ function App() {
         <section>
           <h2>The Mess (cooking training)</h2>
 
-          <p></p>
+          <p>For more information on the Mess, see <a href="https://oldschool.runescape.wiki/w/Mess">The Mess</a> Old School Runescape Wiki article.</p>
 
           <figure>
               <video controls>
                 <source src="/themesscookingpizzasDec12_2024.mp4" type="video/mp4" />
               </video>
 
-          <figcaption>Me cooking servery pineapple pizzas at the Mess south of the Hosidius statue for about 100k cooking xp/hr
-            (with misclicks only because of the lag from running the screen recorder):</figcaption>
+          <figcaption>Cooking servery pineapple pizzas at the Mess south of the Kourend statue for about 100k cooking xp/hr (screen recorder causing lag). Ideally your inventory would be a rune pouch and knife so that you can get 13 bowls and 13 flour to start. Use bowls on sink to fill them, then hold 2 and combine the pots of flour with the water to make pizza dough. Take 13 tomatoes and use it on the dough, then take 13 cheese and use it to make uncooked pizzas. Take 13 pineapple, cook the pizzas, and then slice the pineapple with your knife and use the slices on the cooked pizzas. Turn it in outside the kitchen for about 4800 xp for 13 and hop to the next world as that will maximize XP rates.</figcaption>
 
           </figure>
         
