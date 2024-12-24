08ca526fc21e8545c2c2d9aa31bea4e6b6d41cfe
Kyle Rego <regoky@outlook.com>
2024-12-24 13:01:29 -0800

    CSS improvements

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ function App() {
   return (
     <>
       <div>
-        <h1>Lilgobslayerguides.NET</h1>
+        <section>
+          <h1>Lilgobslayerguides.NET</h1>
 
-        <p>Practical Old School Runescape Ultimate Iron guides for Gielnor's most bankless gamers.</p>
+          <p>Practical Old School Runescape Ultimate Iron guides for the most bankless adventurers.</p>
+        </section>
 
         <section>
           <h2>The Mess (cooking training)</h2>
@@ -18,7 +20,9 @@ function App() {
                 <source src="/themesscookingpizzasDec12_2024.mp4" type="video/mp4" />
               </video>
 
-          <figcaption>Cooking servery pineapple pizzas at the Mess south of the Kourend statue for about 100k cooking xp/hr (screen recorder causing lag). Ideally your inventory would be a rune pouch and knife so that you can get 13 bowls and 13 flour to start. Use bowls on sink to fill them, then hold 2 and combine the pots of flour with the water to make pizza dough. Take 13 tomatoes and use it on the dough, then take 13 cheese and use it to make uncooked pizzas. Take 13 pineapple, cook the pizzas, and then slice the pineapple with your knife and use the slices on the cooked pizzas. Turn it in outside the kitchen for about 4800 xp for 13 and hop to the next world as that will maximize XP rates.</figcaption>
+          <figcaption>
+            Cooking servery pineapple pizzas at the Mess south of the Kourend statue for about 100k cooking xp/hr (screen recorder causing lag). Ideally your inventory would be a rune pouch and knife so that you can get 13 bowls and 13 flour to start. Use bowls on sink to fill them, then hold 2 and combine the pots of flour with the water to make pizza dough. Take 13 tomatoes and use it on the dough, then take 13 cheese and use it to make uncooked pizzas. Take 13 pineapple, cook the pizzas, and then slice the pineapple with your knife and use the slices on the cooked pizzas. Turn it in outside the kitchen for about 4800 xp for 13 and hop to the next world as that will maximize XP rates.
+          </figcaption>
 
           </figure>
         
